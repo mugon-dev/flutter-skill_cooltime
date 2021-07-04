d112d1aa1d04e65a969da5b7586c7bb73e46a3f2
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-07-04 20:23:36 +0900

    bloc 컨트롤러 선언

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:skill_cooltime/src/components/Cooltime_button.dart';
 import 'package:skill_cooltime/src/components/display_view.dart';
+import 'package:skill_cooltime/src/controller/button_controller.dart';
 
-class App extends StatelessWidget {
+// 전역으로 선언
+ButtonController buttonController = ButtonController();
+
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    buttonController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
