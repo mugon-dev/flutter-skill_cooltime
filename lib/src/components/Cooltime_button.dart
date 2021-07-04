0c4a4599a326cf238d9f89aeac2620f4029e17cb
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-07-04 20:59:42 +0900

    streambuilder를 통해 controller에서 데이터를 받아오고 버튼 이벤트에 future.delay를 걸어 쿨타임 적용

--- lib/src/components/Cooltime_button.dart
+++ lib/src/components/Cooltime_button.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:skill_cooltime/src/app.dart';
+import 'package:skill_cooltime/src/controller/button_controller.dart';
 
-class CooltimeButton extends StatelessWidget {
+class CooltimeButton extends StatefulWidget {
   const CooltimeButton({Key? key}) : super(key: key);
+
+  @override
+  _CooltimeButtonState createState() => _CooltimeButtonState();
+}
+
+class _CooltimeButtonState extends State<CooltimeButton> {
+  bool isActive = true;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: GestureDetector(
-        onTap: () {},
+        onTap: _actionButton,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(15),
           child: Container(
             width: 100,
             height: 100,
-            color: Colors.grey.withOpacity(0.5),
+            color: isActive
+                ? Colors.grey.withOpacity(0.5)
+                : Colors.black.withOpacity(0.8),
             child: Center(
               child: Text(
                 "Button",
@@ -24,5 +35,21 @@ class CooltimeButton extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _changeState(bool state) {
+    setState(() {
+      isActive = state;
+    });
+  }
+
+  void _actionButton() {
+    if (isActive) {
+      _changeState(false);
+      buttonController.action(ButtonType.ACTION1);
+      Future.delayed(Duration(seconds: 2), () {
+        _changeState(true);
+      });
+    }
   }
 }
--- lib/src/components/display_view.dart
+++ lib/src/components/display_view.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:skill_cooltime/src/app.dart';
+import 'package:skill_cooltime/src/controller/button_controller.dart';
 
 class DisplayView extends StatelessWidget {
   const DisplayView({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        0.toString(),
-        style: TextStyle(fontSize: 100),
-      ),
+    int i = 0;
+    // button controller 구독
+    return StreamBuilder(
+      stream: buttonController.actionButtonStream,
+      builder: (_, AsyncSnapshot<ButtonType> snapshot) {
+        i++;
+        return Text(
+          i.toString(),
+          style: TextStyle(fontSize: 100),
+        );
+      },
     );
   }
 }
